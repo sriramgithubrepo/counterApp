da634233ed67c4b31ecfec4c59b24344dc75778e
Sriram B <97724465+sriramgithubrepo@users.noreply.github.com>
2025-05-04 15:09:08 +0200

    integrated reporting (#2)
    * integrated reporting related changes and updated readme covering the steps to execute cypress test locally

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl:'http://localhost:3000',
+    setupNodeEvents() {
     },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/results",
+      overwrite: true,
+      html: true,
+      json: true
+    }
   },
 });
