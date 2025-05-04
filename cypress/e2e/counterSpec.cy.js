90b5b1e9047745998d1fefd9c1a4b1dac5b2ee91
Sriram B <97724465+sriramgithubrepo@users.noreply.github.com>
2025-05-04 17:40:34 +0200

    Enhance (#3)
    Added couple of more test in same spec

--- cypress/e2e/counterSpec.cy.js
+++ cypress/e2e/counterSpec.cy.js
@@ -4,12 +4,26 @@ describe('Counter App', () => {
   const counterPage = new CounterPage();
   beforeEach(() => {
     counterPage.visit();
+    counterPage.getCounterValue().should('be.visible');
+    counterPage.getCounterValue().should('have.text', '0');
   });
 
   it('increments the counter when pressing increment button', () => {
-    counterPage.getCounterValue().should('be.visible');
-    counterPage.getCounterValue().should('have.text', '0');
     counterPage.clickIncrement();
     counterPage.getCounterValue().should('have.text', '1');
+  });
+
+  it('decrements the counter when pressing decrement button', () => {
+    counterPage.clickIncrement();
+    counterPage.getCounterValue().should('have.text', '1');
+    counterPage.clickDecrement();
+    counterPage.getCounterValue().should('have.text', '0');
+  });
+
+  it('browser reload reset the counter to initial state', () => {
+    counterPage.clickIncrement();
+    counterPage.getCounterValue().should('have.text', '1');
+    cy.reload();
+    counterPage.getCounterValue().should('have.text', '0');
   });
 });
